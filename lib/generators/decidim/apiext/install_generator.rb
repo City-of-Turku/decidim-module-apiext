3bc9c7f991df77dfab01267428c83a37d8922328
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-09-09 14:30:01 +0300

    Fix the install generator

--- lib/generators/decidim/apiext/install_generator.rb
+++ lib/generators/decidim/apiext/install_generator.rb
@@ -10,7 +10,7 @@ module Decidim
         def add_jwt_secret
           secrets_path = Rails.application.root.join("config", "secrets.yml")
           evaluated_secrets = ERB.new(File.read(secrets_path))
-          secrets = YAML.safe_load(evaluated_secrets.result, [], [], true)
+          secrets = YAML.safe_load(evaluated_secrets.result, aliases: true)
 
           add_secret_to(secrets_path, /^development:/) unless secrets.dig(:development, :secret_key_jwt)
           add_secret_to(secrets_path, /^test:/) unless secrets.dig(:test, :secret_key_jwt)
