7e105340cda3898bb6f006af317f26beb9b9f799
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-09-16 19:46:49 +0300

    Cleanup the comments

--- lib/decidim/apiext/accountability_type_extensions.rb
+++ lib/decidim/apiext/accountability_type_extensions.rb
@@ -2,6 +2,9 @@
 
 module Decidim
   module Apiext
+    # Adds the statuses to the accountability API that may be a core feature in
+    # the future:
+    # https://github.com/decidim/decidim/pull/15189
     module AccountabilityTypeExtensions
       def self.included(type)
         type.field :statuses, [Decidim::Accountability::StatusType], null: false
